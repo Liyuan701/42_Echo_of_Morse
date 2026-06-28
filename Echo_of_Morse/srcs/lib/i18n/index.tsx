796dbf77774293f81016f5628d0098ea77e4afd3
REN <yy@Mac-002.lan>
2026-06-28 19:25:47 +0200

    style: cmt pour i18n

--- Echo_of_Morse/srcs/lib/i18n/index.tsx
+++ Echo_of_Morse/srcs/lib/i18n/index.tsx
@@ -8,7 +8,7 @@ import en from "./en";
 import fr from "./fr";
 import zh from "./zh";
 
-// Language = un entree de la liste: en, fr, zh
+// Language = une entree possible: en, fr ou zh
 //| = ou
 export type Language = "en" | "fr" | "zh";
 
@@ -22,8 +22,8 @@ const dictionaries: Record<Language, dictionaryContent> = { en, fr, zh };
 //======================================== 1. creer un global et met valeur par defaut (createContext) ========================================
 //interface = comme "strcut" en C, mais pour les objets en TypeScript
 interface I18nContextType {
-	language: Language;
-	dictionary: dictionaryContent;
+	language: Language; 				//--> indique la langue
+	dictionary: dictionaryContent;		//--> //--> les contenue dans le fichier pour cette langue
 	setLanguage: (l: Language) => void;//une fonction qui prend une langue et ne retourne rien (void)
 }
 
@@ -36,16 +36,23 @@ const I18nContext = createContext<I18nContextType>({
 });
 
 //======================================== 2. donner des valeur pour un global (useState & provider) ========================================
-//{ children }: { children: React.ReactNode } = un action dans paramete, creer une varibale "children" qui est de type React.ReactNode
+//{ children }: { children: React.ReactNode } = une action dans paramete, creer une varibale "children" qui est de type React.ReactNode
+//React.ReactNode = type qui représente tout ce que React peut afficher dans une page
+//ex: 
+// <I18nProvider>
+//   <App />
+// </I18nProvider>
+//ici, children = <App />
 export function I18nProvider({ children }: { children: React.ReactNode }) {
 
 	//useState = cree une variable geree par React et une fonction pour la modifier
 	//il faut utiliser cette fonction pour que React mette a jour la page quand la variable change
-
-	// const [selectedLanguage, setSelectedLanguage] = useState<Language>("fr");
 	const [selectedLanguage, setSelectedLanguage] = useState<Language>("fr");
 
+	//---------------- 1. lire la langue dans localStorage ----------------
 	useEffect(() => {
+		// localStorage --> un petit stockage du navigateur
+		// getItem --> lit la cle "language"
 		const savedLanguage = window.localStorage.getItem("language");
 
 		if (savedLanguage === "en" || savedLanguage === "fr" || savedLanguage === "zh") {
@@ -53,18 +60,21 @@ export function I18nProvider({ children }: { children: React.ReactNode }) {
 		}
 	}, []);
 
+	//---------------- 2. lorsque user change la langue ----------------
 	function changeLanguage(language: Language) {
 		setSelectedLanguage(language);
+		//mettre par key value --> "language": language dans localStorage
+		//pour aider de memoire
 		window.localStorage.setItem("language", language);
 	}
 
 	return (
-		//I18nContext.Provider = on utilise le box global pour fournir les valeurs
-		//value = les valeurs que on veut fournir
+		//envoyer les donnes pour tous (le projet)
+		//I18nContext.Provider value = --> on utilise le box global pour fournir les valeurs
 		<I18nContext.Provider value={{ 
-			language: selectedLanguage,
-			dictionary: dictionaries[selectedLanguage], 
-			setLanguage: changeLanguage,
+			language: selectedLanguage,					//langue actuelle
+			dictionary: dictionaries[selectedLanguage], //dictionaire pour cette langue
+			setLanguage: changeLanguage,				//une fonction pour changer de la langue
 		}}>
 		{children}
 		</I18nContext.Provider>
